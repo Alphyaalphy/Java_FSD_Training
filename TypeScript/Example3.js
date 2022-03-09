48095851fd2f22e80d9e2d0e26a438176244511f
Alphyaalphy <62606970+Alphyaalphy@users.noreply.github.com>
2022-03-09 09:29:31 +0530

    Add files via upload

---
+++ TypeScript/Example3.js
@@ -0,0 +1,11 @@
+var grade = "A";
+switch (grade) {
+    case "A": {
+        console.log("Excellent");
+        break;
+    }
+    case "B": {
+        console.log("Good");
+        break;
+    }
+}
---
+++ TypeScript/Example3.ts
@@ -0,0 +1,11 @@
+var grade:string = "A"; 
+switch(grade) { 
+   case "A": { 
+      console.log("Excellent"); 
+      break; 
+   } 
+   case "B": { 
+      console.log("Good"); 
+      break; 
+   } 
+}
